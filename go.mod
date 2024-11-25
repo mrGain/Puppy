689a4e266aa58170f896803688ae81f00563a21b
Rakesh Gain <rakeshgain47681@gmail.com>
2024-11-25 23:10:50 +0530

    dog dependency added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mrGain/Puppy
 
 go 1.22.2
+
+require github.com/mrGain/dmgmt_dog v0.0.0-20241125172427-6632b73fda55
--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package puppy
 
+import (
+	"github.com/mrGain/dmgmt_dog"
+)
+
 func Bark() string {
 	return "woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Bark())
 }
